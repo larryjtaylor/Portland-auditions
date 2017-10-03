32748b5372c4e44ee201118ea6acf31c11079f6f
Larry <larryjamestaylor@gmail.com>
2017-10-03 13:32:31 -0700

    add image and align in grid

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Image from "./../img/portlandSign.jpg";
 
 function Welcome(){
+
+  var welcome={
+    marginTop: "125px",
+    marginLeft: "5%",
+  }
+  var imageStyle = {
+    width: "100%"
+  }
+
   return (
-    <div>
-      <h1>Welcome to Portland"s Theatre home!</h1>
-      <p>Here, you will be able to explore some of the amazing theatre companies Portland has to offer. It is not always easy to keep up on upcoming auditions. This site offers a centralized hub to know what shows are coming down the line and how to get in on the action.</p>
-    </div>
+      <div style={welcome}>
+        <Container>
+          <Row>
+            <Col md={5}>
+              <img src={Image} style={imageStyle}></img>
+            </Col>
+            <Col md={7}>
+              <h1>Welcome to Portland's Theatre Audition home!</h1>
+              <h4>Here, you will be able to explore some of the amazing theatre companies Portland has to offer. It is not always easy to keep up on upcoming auditions. This site offers a centralized hub to know what shows are coming down the line and how to get in on the action.</h4>
+            </Col>
+          </Row>
+        </Container>
+      </div>
   );
 }
 
