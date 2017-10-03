7f81a3e3c5cbb1b22fb952d06a2fbef3ac283b3b
Larry <larryjamestaylor@gmail.com>
2017-10-03 15:25:33 -0700

    finish CRUD functionality by making it possible to remove companies

---
+++ src/components/Admin.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import CompanyList from "./CompanyList";
+import { connect } from 'react-redux';
+import c from './../constants';
+
+class Admin extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClosingCompany = this.handleClosingCompany.bind(this);
+  }
+
+  handleClosingCompany(companyId) {
+    const { dispatch } = this.props;
+    const action = {
+      type: c.CLOSE_COMPANY,
+      companyId: companyId,
+    }
+    dispatch(action);
+  }
+
+  render() {
+    console.log("Current Path:", this.props.location.pathname);
+    return (
+      <div>
+      <h3>This is the Admin page!</h3>
+      <CompanyList
+        companyList={this.props.adminMasterCompanyList}
+        currentRoute={this.props.location.pathname}
+        handleClosingCompany={this.handleClosingCompany}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    adminMasterCompanyList : state
+  }
+}
+
+export default connect(mapStateToProps)(Admin);
