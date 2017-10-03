97b1752c11d69540d13cb8efb58fcb8be589344f
Larry <larryjamestaylor@gmail.com>
2017-10-03 13:34:45 -0700

    create auditions component where audition calendar will live

---
+++ src/components/Auditions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BigCalendar from 'react-big-calendar';
+
+const Auditions {
+  <div>
+    <BigCalendar
+      events={myEventsList}
+      startAccessor='startDate'
+      endAccessor='endDate'
+    />
+  </div>
+}
