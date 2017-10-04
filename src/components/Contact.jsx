9cc45f6dbc1e35f6f4991500d245466251e7a887
larry <larryjamestaylor@gmail.com>
2017-10-03 17:35:08 -0700

    add contact compnopent

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact(props) {
+
+var contact = {
+  marginTop: "115px",
+  marginLeft: "5%"
+}
+  return (
+    <div>
+      <h1 style={contact}>Contact:</h1>
+      <hr/>
+    </div>
+  );
+}
+
+export default Contact;
