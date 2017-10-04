436dccdc3958bd65a25acb0d06c307283753ad6c
larry <larryjamestaylor@gmail.com>
2017-10-03 17:28:29 -0700

    add component for footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Footer(props) {
+
+  var navbar={
+    height: "50px",
+    position: "fixed",
+    width: "100%",
+    bottom: "0%",
+    backgroundColor: "black",
+    zIndex: "1",
+    textAlign: "center"
+  }
+
+  var linkColor={
+    color: "white",
+  }
+  return (
+    // <div className="navbar navbar-default" style={navbar}>
+    //  <div className="container-fluid">
+       <div className="navbar-footer">
+         <h4 style={navbar}><NavLink to="/admin" style={linkColor}>Admin</NavLink></h4>
+       </div>
+    //  </div>
+  //  </div>
+  );
+}
+
+export default Footer;
